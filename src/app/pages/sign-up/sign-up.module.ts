48544fd261fc35aabadd49b3b30bed180e321553
Henrique Viana <hsantos@lopes.com.br>
2021-07-07 16:19:45 -0300

    WIP - update signin signup

--- src/app/pages/sign-up/sign-up.module.ts
+++ src/app/pages/sign-up/sign-up.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrMaskerModule } from 'br-mask';
+
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,6 +15,7 @@ import { SignUpPage } from './sign-up.page';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,  
+    BrMaskerModule,
     IonicModule,
     SignUpPageRoutingModule
   ],
